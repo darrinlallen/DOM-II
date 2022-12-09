d4c4696e6ba995cc3810d05b5f58e547dfdb7530
darrin allen <darrin.lovard.allen@gmail.com>
2022-12-09 14:32:02 -0800

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 import './less/index.less'
 
 // Your code goes here!
+
+//1. load
+window.onload  = function(evt) {
+console.log (`event ${evt.type} fired Ready to go`);
+const heading = document.querySelector('h1'); 
+heading.textContent = "Ready to go"
+
+//2. copy
+window.addEventListener('copy', () => {
+    navigator.clipboard.readText()
+    .then(text => {
+        heading.textContent += text;
+    })
+})
+// 3. click 
+document.body.addEventListener('click' , evt => {
+    evt.target.classList.toggle('mirror')   
+})
+
+//4. double click
+document.body.addEventListener('dblclick' , evt => {
+    evt.target.outerHTML = '';   
+})
+
+//5. keydown
+window.addEventListener('keydown', evt => {
+ if (evt.key == 6){
+    document.body.innerHTML = ''
+ }   
+})
+
+//6. mouse move
+document.body.addEventListener('mousemove', evt => {
+const {clientX, clientY} = evt;
+
+
+})
+// 7- mouse enter
+// 8 - mouse leave
+const destinations = document.querySelectorAll('.destination')
+for (let destination of destinations){
+    destination.addEventListener('mouseenter', evt =>{
+        destination.style.fontWeight = 'bold'
+
+    })
+    destination.addEventListener('mouseleave', evt =>{
+        destination.style.fontWeight = 'initial'
+})
+}
+
+}
